388400e4aefce86e4ec3ea14072b432c01002ede
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-11-13 01:54:36 +0530

    Update variables.tf

--- templatesProjectIAMBinding/variables.tf
+++ templatesProjectIAMBinding/variables.tf
@@ -2,7 +2,21 @@ variable "project_id" {
     default     = "kd-project-437712"
 type = string
 }
-variable "project_iam_binding" {
+/*variable "project_iam_binding" {
   sensitive = false
 type = string
+}*/
+
+variable "project_iam_binding" {
+  type = map(object({
+    project_id = string
+    delete     = bool
+    role       = list(string)
+    members    = list(string)
+    condition  = list(object({
+      title       = string
+      description = string
+      expression  = string
+    }))
+  }))
 }
