302078eea19ec717ebf52d98abfcbff156f14fae
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-11-13 00:53:47 +0530

    Create project-iam-binding.tf

---
+++ templatesProjectIAMBinding/project-iam-binding.tf
@@ -0,0 +1,10 @@
+module "project_iam_binding" {
+  for_each     = {
+    for k, v in try(var.project_iam_binding,{}): k => v if v.delete != true
+  }
+  source         = "./terraform-google-cloud-project-iam-binding"
+  project_id     = each.key
+  roles          = each.value.role
+  members        = each.value.members
+  condition      = each.value.condition
+}
