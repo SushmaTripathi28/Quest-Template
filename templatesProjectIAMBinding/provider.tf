d627525271c15d092612a4a15be811071e5d6fb1
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-11-18 23:12:43 +0530

    Update provider.tf

--- templatesProjectIAMBinding/provider.tf
+++ templatesProjectIAMBinding/provider.tf
@@ -1,7 +1,7 @@
 #..................................... Provider Block ......................................#
 provider "google" {
     project         = var.project_id
-   # access_token    = var.access_token
+    # access_token    = var.access_token
 }
 provider "google-beta" {
     project         = var.project_id
