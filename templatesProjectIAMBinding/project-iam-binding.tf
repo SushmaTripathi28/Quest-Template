613e1baed009407758ab229bcefb8bbf58b1b3a8
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-11-19 01:06:50 +0530

    Update project-iam-binding.tf

--- templatesProjectIAMBinding/project-iam-binding.tf
+++ templatesProjectIAMBinding/project-iam-binding.tf
@@ -2,7 +2,7 @@ module "project_iam_binding" {
   for_each     = {
     for k, v in try(var.project_iam_binding,{}): k => v if v.delete != true
   }
-  source         = "git::https://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@github.com/SushmaTripathi28/Quest-Module.git//terraform-google-cloud-project-iam-binding?ref=v1.0.0"
+  source         = "git::https://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@github.com/SushmaTripathi28/Quest-Module.git//terraform-google-cloud-project-iam-binding?ref=v1.0.0"
   project_id     = each.key
   roles          = each.value.role
   members        = each.value.members
